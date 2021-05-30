3433f1107175c2d964a424caf6f84c0ab501b016
Shamil Habeeb <shamilhabeeb5@gmail.com>
2021-05-30 10:05:20 +0530

    നോക്കേണ്ട ഡാ ഉണ്ണി ഇത് ഞാനല്ല

--- bot.py
+++ bot.py
@@ -1,8 +1,5 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# (c) Shrimadhav U K
 
-# the logging things
+
 import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
